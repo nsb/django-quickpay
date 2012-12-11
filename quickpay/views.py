48d39f90e01dac8af5dc8823db1b650d46ae80c0
Jared Morse <jarcoal@gmail.com>
2012-12-11 13:43:50 -0800

    csrf_exempt callback view

--- quickpay/views.py
+++ quickpay/views.py
@@ -1,5 +1,7 @@
 from models import QuickpayTransaction
 from django.views.generic.edit import BaseCreateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 class BaseQuickpayCallback(BaseCreateView):
 	"""
@@ -7,3 +9,7 @@ class BaseQuickpayCallback(BaseCreateView):
 	"""
 	http_method_names = ('post',)
 	model = QuickpayTransaction
+
+	@method_decorator(csrf_exempt)
+	def dispatch(self, *args, **kwargs):
+		return super(BaseQuickpayCallback, self).dispatch(*args, **kwargs)
