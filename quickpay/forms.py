a8a87f6d465a61da77ef1bc543b84f538921d512
Jared Morse <jarcoal@gmail.com>
2012-12-12 15:07:13 -0800

    apply request_md5check to form

--- quickpay/forms.py
+++ quickpay/forms.py
@@ -1,26 +1,43 @@
 from django import forms
 from django.conf import settings
+from utils import request_md5check
 
 class QuickpayForm(forms.Form):
     protocol = forms.IntegerField(widget=forms.HiddenInput, initial=6)
-    language = forms.CharField(widget=forms.HiddenInput, initial=settings.LANGUAGE_CODE)
-    cardtypelock = forms.CharField(widget=forms.HiddenInput, initial='creditcard')
-    merchant = forms.CharField(widget=forms.HiddenInput)
-    description = forms.CharField(widget=forms.HiddenInput)
-    continueurl = forms.URLField(widget=forms.HiddenInput)
-    cancelurl = forms.URLField(widget=forms.HiddenInput)
-    callbackurl = forms.URLField(widget=forms.HiddenInput)
-    autocapture = forms.IntegerField(widget=forms.HiddenInput, initial=0)
-    autofee = forms.IntegerField(widget=forms.HiddenInput, initial=0)
-    group = forms.IntegerField(widget=forms.HiddenInput)
-    testmode = forms.IntegerField(widget=forms.HiddenInput, initial=int(settings.DEBUG))
-    forcemobile = forms.IntegerField(widget=forms.HiddenInput, initial=0)
-    deadline = forms.IntegerField(widget=forms.HiddenInput)
-
     msgtype = forms.IntegerField(widget=forms.HiddenInput, initial='authorize')
+    merchant = forms.CharField(widget=forms.HiddenInput)
+    language = forms.CharField(widget=forms.HiddenInput, initial=settings.LANGUAGE_CODE)
     ordernumber = forms.CharField(widget=forms.HiddenInput)
     amount = forms.IntegerField(widget=forms.HiddenInput)
     currency = forms.CharField(widget=forms.HiddenInput, initial='USD')
-    splitpayment = forms.IntegerField(widget=forms.HiddenInput, initial=0)
-    cardhash = forms.IntegerField(widget=forms.HiddenInput, initial=1)
+    continueurl = forms.URLField(widget=forms.HiddenInput)
+    cancelurl = forms.URLField(widget=forms.HiddenInput)
+
+    callbackurl = forms.URLField(widget=forms.HiddenInput, required=False)
+    autocapture = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    autofee = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    cardtypelock = forms.CharField(widget=forms.HiddenInput, required=False)
+    description = forms.CharField(widget=forms.HiddenInput, required=False)
+    group = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    testmode = forms.IntegerField(widget=forms.HiddenInput, initial=int(settings.DEBUG), required=False)
+    splitpayment = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    forcemobile = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    deadline = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    cardhash = forms.IntegerField(widget=forms.HiddenInput, required=False)
+
     md5check = forms.CharField(widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        secret = kwargs.pop('secret', None)
+        super(QuickpayForm, self).__init__(*args, **kwargs)
+
+        if secret:
+            self.compute_md5check(secret)
+
+    def compute_md5check(self, secret):
+        data = {}
+
+        for field in self:
+            data[field.name] = field.value()
+
+        self.fields['md5check'].initial = request_md5check(data, secret)
