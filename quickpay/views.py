7850cc9fe294ec7315b642b1454e5b9008119367
Jared Morse <jarcoal@gmail.com>
2012-12-12 15:26:00 -0800

    form_valid/form_invalid methods on callback view

--- quickpay/views.py
+++ quickpay/views.py
@@ -2,6 +2,7 @@ from models import QuickpayTransaction
 from django.views.generic.edit import BaseCreateView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 class BaseQuickpayCallback(BaseCreateView):
 	"""
@@ -13,3 +14,10 @@ class BaseQuickpayCallback(BaseCreateView):
 	@method_decorator(csrf_exempt)
 	def dispatch(self, *args, **kwargs):
 		return super(BaseQuickpayCallback, self).dispatch(*args, **kwargs)
+
+	def form_valid(self, form):
+		form.save()
+		return HttpResponse('OK')
+
+	def form_invalid(self, form):
+		return HttpResponse('FAIL', status=400)
